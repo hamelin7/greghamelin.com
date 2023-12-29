cb87da4b2ecd96cf679b19e2b459bb35ffbf8e65
Greg Hamelin <132278697+hamelin7@users.noreply.github.com>
2023-12-29 14:07:13 -0500

    add flexboxes and update styling

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const titles = [
+    'Pizza Fanatic',
+    'SysAdmin',
     'Developer',
-    'Programmer',
-    'Designer'
 ];
 
 let titlesIndex = 0;
@@ -14,3 +14,37 @@ const changeTitle = function(){
 };
 
 setInterval(changeTitle, 2500);
+
+document.addEventListener('DOMContentLoaded', function() {
+    const flexBoxes = document.querySelectorAll('.flex-box');
+  
+    flexBoxes.forEach(function(box) {
+      box.addEventListener('mouseover', function() {
+        flexBoxes.forEach(function(otherBox) {
+          if (otherBox !== box) {
+            otherBox.classList.add('darken');
+          }
+        });
+      });
+  
+      box.addEventListener('mouseout', function() {
+        flexBoxes.forEach(function(otherBox) {
+          otherBox.classList.remove('darken');
+        });
+      });
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const flexBoxes = document.querySelectorAll('.flex-box');
+  
+    flexBoxes.forEach(function(box) {
+      box.addEventListener('mouseover', function() {
+        document.body.classList.add('darken');
+      });
+  
+      box.addEventListener('mouseout', function() {
+        document.body.classList.remove('darken');
+      });
+    });
+  });
